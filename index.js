dc97302f9f50a4baec70b115149e5da8c5dbaa3a
shahira-sohail <shahirasohail10@gmail.com>
2026-07-25 16:34:07 +0530

    Add files via upload
    Added Sprint02 code

---
+++ index.js
@@ -0,0 +1,184 @@
+const salaryForm = document.getElementById("salaryForm");
+const salaryInput = document.getElementById("salary");
+
+const salaryDisplay = document.getElementById("salaryDisplay");
+const balanceDisplay = document.getElementById("balanceDisplay");
+
+//expense
+const expenseForm = document.getElementById("expenseForm");
+const expenseNameInput = document.getElementById("expenseName");
+const expenseAmountInput = document.getElementById("expenseAmount");
+
+const expenseDisplay = document.getElementById("expenseDisplay");
+const expenseList = document.getElementById("expenseList");
+
+const salaryError = document.getElementById("salaryError");
+const expenseNameError = document.getElementById("expenseNameError");
+const expenseAmountError = document.getElementById("expenseAmountError");
+
+const chartCanvas = document.getElementById("expenseChart");
+let expenseChart;
+
+
+
+let totalSalary = 0;
+let totalExpenses = 0;
+let remainingBalance = 0;
+let expenses = [];
+
+//stopping refresh of the page when submitting a form
+salaryForm.addEventListener("submit",function(event){
+    event.preventDefault();
+
+    const salary = Number(salaryInput.value);
+    salaryError.textContent = "";
+    if(salary <= 0){
+        salaryError.textContent = "Salary must be greater than 0.";
+         return;
+    }
+    totalSalary = salary;
+    remainingBalance = salary;
+
+    //updating the dashboard
+    salaryDisplay.textContent = `₹${totalSalary}`;
+    balanceDisplay.textContent = `₹${remainingBalance}`;
+
+    saveToLocalStorage();
+    renderChart();
+});
+
+expenseForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const expenseName = expenseNameInput.value;
+    const expenseAmount = Number(expenseAmountInput.value);
+    expenseNameError.textContent = "";
+    expenseAmountError.textContent = "";
+
+    if(expenseName.trim() === ""){
+        expenseNameError.textContent = "Expense name is required.";
+        return;
+    }
+
+    if(expenseAmount <= 0){
+        expenseAmountError.textContent = "Expense amount must be greater than 0.";
+        return;
+    }
+
+    const expense = {
+        id: Date.now(),//creating a unique id
+        name: expenseName,
+        amount: expenseAmount
+    };
+    expenses.push(expense);
+    totalExpenses += expenseAmount;
+
+    remainingBalance = totalSalary - totalExpenses;
+
+    expenseDisplay.textContent = `₹${totalExpenses}`;
+    balanceDisplay.textContent = `₹${remainingBalance}`;
+
+    saveToLocalStorage();
+    renderExpenses();
+    renderChart();
+    expenseNameInput.value = "";
+    expenseAmountInput.value = "";
+});
+
+function saveToLocalStorage() {
+    localStorage.setItem(
+        "salary",
+        JSON.stringify(totalSalary)
+    );
+
+    localStorage.setItem(
+        "expenses",
+        JSON.stringify(expenses)
+    );
+}
+
+function loadFromLocalStorage(){
+    const savedSalary = JSON.parse(localStorage.getItem("salary"));
+    const savedExpenses = JSON.parse(localStorage.getItem("expenses"));
+    if(savedSalary){totalSalary = savedSalary;}
+    if(savedExpenses){expenses = savedExpenses;}
+
+    totalExpenses = 0;
+    expenses.forEach(function(expense){totalExpenses += expense.amount;});
+    remainingBalance = totalSalary - totalExpenses;
+    salaryDisplay.textContent = `₹${totalSalary}`;
+    expenseDisplay.textContent = `₹${totalExpenses}`;
+    balanceDisplay.textContent = `₹${remainingBalance}`;
+    expenseList.innerHTML = "";
+    renderExpenses();
+    renderChart();
+}
+
+function renderExpenses() {
+    expenseList.innerHTML = "";
+    expenses.forEach(function(expense) {
+        const expenseItem = document.createElement("div");
+        expenseItem.classList.add("expense-item");
+        expenseItem.innerHTML = `
+            <span>${expense.name}</span>
+            <div>
+                <span class="expense-price">₹${expense.amount}</span>
+                <button class="delete-btn" data-id="${expense.id}">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+        `;
+        expenseList.appendChild(expenseItem);    
+    });           
+} 
+
+function renderChart() {
+    if (expenseChart) {
+        expenseChart.destroy();
+    }
+
+    expenseChart = new Chart(chartCanvas, {
+        type: "pie",
+        data: {
+            labels: [
+                 "Remaining Balance",
+                 "Total Expenses"
+            ],
+            datasets: [
+                {
+                    data: [
+                        remainingBalance,
+                        totalExpenses
+                    ],
+                    backgroundColor: [
+                         "#22c55e",
+                         "#ef4444"
+                    ]
+                }
+            ]
+        }
+    });
+}
+
+loadFromLocalStorage();
+
+expenseList.addEventListener("click", function(event){
+    if(event.target.closest(".delete-btn")){
+       const button = event.target.closest(".delete-btn");
+       const id = Number(button.dataset.id);
+        expenses = expenses.filter(function(expense){
+            return expense.id !== id;
+        });
+        totalExpenses = 0;
+        expenses.forEach(function(expense){
+            totalExpenses += expense.amount;
+        });
+        remainingBalance = totalSalary - totalExpenses;
+        expenseDisplay.textContent = `₹${totalExpenses}`;
+        balanceDisplay.textContent = `₹${remainingBalance}`;
+        saveToLocalStorage();
+        renderExpenses();
+        renderChart();
+    }
+});
+
+
